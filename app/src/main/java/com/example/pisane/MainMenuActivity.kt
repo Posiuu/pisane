64716f2e74521912199d091484eab14b253306e3
Posiuu <piotr.krol505@gmail.com>
2021-02-17 17:30:15 +0100

    Add main_menu layout header

---
+++ app/src/main/java/com/example/pisane/MainMenuActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pisane
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+}
