0978b7af22605f8d05eba27d6bac634ba5da4d09
Posiuu <piotr.krol505@gmail.com>
2022-11-01 20:54:12 +0100

    Login/Register Functionality

---
+++ app/src/main/java/com/example/pisane/activity/RegisterActivity.kt
@@ -0,0 +1,68 @@
+package com.example.pisane.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pisane.databinding.ActivityRegisterBinding
+import com.example.pisane.other.BackgroundWorker
+import com.example.pisane.other.ResultStatus
+
+class RegisterActivity : AppCompatActivity() {
+
+    private val activity = this@RegisterActivity
+    private lateinit var binding: ActivityRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRegister.alpha = 0f;
+        binding.btnRegister.animate().alpha(1f).duration = 1500;
+
+        binding.btnRegister.setOnClickListener {
+            binding.btnRegister.alpha = 0f;
+            binding.btnRegister.animate().alpha(1f).duration = 1500;
+            register(binding.etUsername.text.toString(), binding.etPassword.text.toString())
+        }
+
+        binding.btnBackToLogin.setOnClickListener {
+            binding.btnBackToLogin.alpha = 0f;
+            binding.btnBackToLogin.animate().alpha(1f).duration = 1500;
+            backToLogin()
+        }
+    }
+
+    private fun register(username: String, password: String) {
+        val type = "register"
+        val backgroundWorker = BackgroundWorker(this)
+        backgroundWorker.execute(type, username, password)
+
+        val result = backgroundWorker.get().toString()
+        when (result){
+            ResultStatus.SUCCESS.name -> registerSuccessful()
+            ResultStatus.FAIL.name -> registerFailed()
+        }
+    }
+
+    private fun registerSuccessful() {
+        emptyInputEditText()
+        Toast.makeText(this, "Nowe konto utworzone.", Toast.LENGTH_LONG).show()
+    }
+
+    private fun registerFailed() {
+        Toast.makeText(this, "Nie udało się zarejestrować. Prawdopodobnie wybrana nazwa użytkownika jest już zajęta.", Toast.LENGTH_LONG).show()
+    }
+
+    private fun backToLogin() {
+        val accountsIntent = Intent(activity, LoginActivity::class.java)
+        emptyInputEditText()
+        startActivity(accountsIntent)
+    }
+
+    private fun emptyInputEditText() {
+        binding.etUsername.text = null
+        binding.etPassword.text = null
+    }
+}
---
+++ app/src/main/java/com/example/pisane/activity/SettingsActivity.kt
@@ -0,0 +1,44 @@
+package com.example.pisane.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pisane.data.shared_preferences_manager.PREF_GAME
+import com.example.pisane.data.shared_preferences_manager.PREF_USERNAME
+import com.example.pisane.data.shared_preferences_manager.SharedPreferencesManager
+import com.example.pisane.databinding.ActivitySettingsBinding
+
+class SettingsActivity : AppCompatActivity() {
+
+    private val activity = this@SettingsActivity
+    private lateinit var binding: ActivitySettingsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogOut.alpha = 0f;
+        binding.btnLogOut.animate().alpha(1f).duration = 1500;
+
+        binding.btnLogOut.setOnClickListener {
+            binding.btnLogOut.alpha = 0f;
+            binding.btnLogOut.animate().alpha(1f).duration = 1500;
+            logout()
+        }
+    }
+
+    private fun logout() {
+        val sharedPreferencesManager = SharedPreferencesManager(this)
+        val emptyUsername = ""
+        sharedPreferencesManager.putObject(emptyUsername, PREF_USERNAME)
+
+        // delete saved game so new logged in person won't be able to access it
+        sharedPreferencesManager.putObject(null, PREF_GAME)
+
+        val intent = Intent(activity, LoginActivity::class.java)
+        startActivity(intent)
+        Toast.makeText(this, "Wylogowano poprawnie.", Toast.LENGTH_LONG).show()
+    }
+}
